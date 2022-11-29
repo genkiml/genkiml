b8f04815536603c1e2a1f63bf2732645337691d9
Daniel Gretarsson <danielgretars@gmail.com>
2022-11-29 13:42:47 +0000

    getting there...

--- genkiml/cmake/convert_model.cmake
+++ genkiml/cmake/convert_model.cmake
@@ -1,10 +1,15 @@
 function (genkiml_convert_model model_filepath output_path)
+    if (APPLE AND ${CMAKE_SYSTEM_PROCESSOR} STREQUAL arm64)
+        set(APPLE_M1 TRUE)
+    else()
+        set(APPLE_M1 FALSE)
+    endif()
+
     find_package(Python COMPONENTS Interpreter)
 
     if (Python_FOUND)
         set(venv ${CMAKE_CURRENT_BINARY_DIR}/.venv)
         message("Creating virtual environment at ${venv}")
-
         execute_process(COMMAND ${Python_EXECUTABLE} -m venv ${venv})
 
         if (MSVC)
@@ -16,29 +21,17 @@ function (genkiml_convert_model model_filepath output_path)
 
         set(genkiml_root ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../../)
 
-        if (APPLE AND ${CMAKE_SYSTEM_PROCESSOR} STREQUAL arm64)
-            set(APPLE_M1 TRUE)
-        else()
-            set(APPLE_M1 FALSE)
-        endif()
-
-        message("CMAKE_SYSTEM_NAME: ${CMAKE_SYSTEM_NAME}")
-        message("CMAKE_SYSTEM_PROCESSOR: ${CMAKE_SYSTEM_PROCESSOR}")
-        message("APPLE: ${APPLE}")
-        message("APPLE_M1: ${APPLE_M1}")
+        get_filename_component(model_name ${model_filepath} NAME_WE)
+        set(model_out_path ${output_path}/${model_name})
+        file(MAKE_DIRECTORY ${model_out_path})
+        cmake_path(CONVERT ${model_out_path} TO_NATIVE_PATH_LIST model_out_path_native)
+        cmake_path(CONVERT ${model_filepath} TO_NATIVE_PATH_LIST model_path_native)
 
         if (APPLE_M1)
             set(requirements_txt ${genkiml_root}/requirements-m1.txt)
         else ()
             set(requirements_txt ${genkiml_root}/requirements.txt)
         endif ()
-
-        get_filename_component(model_name ${model_filepath} NAME_WE)
-
-        set(model_out_path ${output_path}/${model_name})
-        file(MAKE_DIRECTORY ${model_out_path})
-        cmake_path(CONVERT ${model_out_path} TO_NATIVE_PATH_LIST model_out_path_native)
-        cmake_path(CONVERT ${model_filepath} TO_NATIVE_PATH_LIST model_path_native)
 
         execute_process(COMMAND ${Python_EXECUTABLE} -m pip install -r ${requirements_txt})
 
