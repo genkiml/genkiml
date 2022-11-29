6716ddf83d7133739c5dc1969a8a8f54792a07cb
Daniel Gretarsson <danielgretars@gmail.com>
2022-11-29 14:40:17 +0000

    genkiml fails are fatal

--- genkiml/cmake/convert_model.cmake
+++ genkiml/cmake/convert_model.cmake
@@ -47,7 +47,7 @@ function (genkiml_convert_model model_filepath output_path)
         cmake_path(CONVERT ${model_out_path} TO_NATIVE_PATH_LIST model_out_path_native)
         cmake_path(CONVERT ${model_filepath} TO_NATIVE_PATH_LIST model_path_native)
 
-        execute_process(COMMAND ${Python_EXECUTABLE} ${genkiml_root}/genkiml.py ${model_path_native} --model-only --output-path ${model_out_path_native})
+        execute_process(COMMAND ${Python_EXECUTABLE} ${genkiml_root}/genkiml.py ${model_path_native} --model-only --output-path ${model_out_path_native} COMMAND_ERROR_IS_FATAL ANY)
 
         file(RENAME ${model_out_path}/model.onnx ${output_path}/${model_name}.onnx)
         file(REMOVE_RECURSE ${model_out_path})
