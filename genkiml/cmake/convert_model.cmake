cf6a649afbaebbffdb86ba278f9e79200f25a0ff
Daniel Gretarsson <danielgretars@gmail.com>
2022-11-29 14:02:26 +0000

    ...

--- genkiml/cmake/convert_model.cmake
+++ genkiml/cmake/convert_model.cmake
@@ -8,16 +8,37 @@ function (genkiml_convert_model model_filepath output_path)
     find_package(Python COMPONENTS Interpreter)
 
     if (Python_FOUND)
-        set(venv ${CMAKE_CURRENT_BINARY_DIR}/.venv)
-        message("Creating virtual environment at ${venv}")
-        execute_process(COMMAND ${Python_EXECUTABLE} -m venv ${venv})
+        set(venv ${CMAKE_BINARY_DIR}/.venv)
 
-        if (MSVC)
-            cmake_path(CONVERT ${venv}/Scripts/activate.bat TO_NATIVE_PATH_LIST venv_activate_bat)
-            execute_process(COMMAND ${venv_activate_bat})
-        else ()
-            execute_process(COMMAND source ${venv}/bin/activate)
-        endif ()
+        if (NOT EXISTS ${venv})
+            message("Creating virtual environment at ${venv}")
+            execute_process(COMMAND ${Python_EXECUTABLE} -m venv ${venv})
+
+            if (MSVC)
+                cmake_path(CONVERT ${venv}/Scripts/activate.bat TO_NATIVE_PATH_LIST venv_activate_bat)
+                execute_process(COMMAND ${venv_activate_bat})
+            else ()
+                execute_process(COMMAND source ${venv}/bin/activate)
+            endif ()
+
+            if (APPLE_M1)
+                set(requirements_txt ${genkiml_root}/requirements-m1.txt)
+            else ()
+                set(requirements_txt ${genkiml_root}/requirements.txt)
+            endif ()
+
+            execute_process(COMMAND ${Python_EXECUTABLE} -m pip install -r ${requirements_txt})
+
+            if (APPLE_M1)
+                set(builder_py "${Python_SITELIB}/google/protobuf/internal/builder.py")
+
+                if (NOT EXISTS ${builder_py})
+                    set(builder_py_url "https://raw.githubusercontent.com/protocolbuffers/protobuf/main/python/google/protobuf/internal/builder.py")
+                    message("Getting builder.py from ${builder_py_url}")
+                    execute_process(COMMAND wget ${builder_py_url} -O ${builder_py})
+                endif ()
+            endif()
+        endif()
 
         set(genkiml_root ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../../)
 
@@ -26,24 +47,6 @@ function (genkiml_convert_model model_filepath output_path)
         file(MAKE_DIRECTORY ${model_out_path})
         cmake_path(CONVERT ${model_out_path} TO_NATIVE_PATH_LIST model_out_path_native)
         cmake_path(CONVERT ${model_filepath} TO_NATIVE_PATH_LIST model_path_native)
-
-        if (APPLE_M1)
-            set(requirements_txt ${genkiml_root}/requirements-m1.txt)
-        else ()
-            set(requirements_txt ${genkiml_root}/requirements.txt)
-        endif ()
-
-        execute_process(COMMAND ${Python_EXECUTABLE} -m pip install -r ${requirements_txt})
-
-        if (APPLE_M1)
-            set(builder_py "${Python_SITELIB}/google/protobuf/internal/builder.py")
-
-            if (NOT EXISTS ${builder_py})
-                set(builder_py_url "https://raw.githubusercontent.com/protocolbuffers/protobuf/main/python/google/protobuf/internal/builder.py")
-                message("Getting builder.py from ${builder_py_url}")
-                execute_process(COMMAND wget ${builder_py_url} -O ${builder_py})
-            endif ()
-        endif()
 
         execute_process(COMMAND ${Python_EXECUTABLE} ${genkiml_root}/genkiml.py ${model_path_native} --model-only --output-path ${model_out_path_native})
 
