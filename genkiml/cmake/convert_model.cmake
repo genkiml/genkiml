fd957be4f147d93add0f90bfd84dd380c9f6f5e8
Daniel Gretarsson <daniel@genkiinstruments.com>
2022-11-30 13:00:28 +0000

    .

--- genkiml/cmake/convert_model.cmake
+++ genkiml/cmake/convert_model.cmake
@@ -5,35 +5,36 @@ function (genkiml_convert_model model_filepath output_path)
         set(APPLE_M1 FALSE)
     endif()
 
+    set(Python_FIND_VIRTUALENV FIRST)
     find_package(Python COMPONENTS Interpreter)
 
     if (Python_FOUND)
         set(genkiml_root ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../..)
 
-        # Github actions can take care of caching pip dependencies
-        # Also, this virtualenv approach doesn't seem to work properly there...
-        if (NOT $ENV{GITHUB_ACTIONS})
-            set(venv ${CMAKE_BINARY_DIR}/.venv)
-
-            if (NOT EXISTS ${venv})
-                message("Creating virtual environment at ${venv}")
-                execute_process(COMMAND ${Python_EXECUTABLE} -m venv ${venv})
-            endif()
-
-            # A trick to make sure cmake uses the virtual environment we've created
-            set(ENV{VIRTUAL_ENV} ${venv})
-            set(Python_FIND_VIRTUALENV FIRST)
-            unset (Python_EXECUTABLE)
-            find_package(Python COMPONENTS Interpreter)
-
-            if (APPLE_M1)
-                set(requirements_txt ${genkiml_root}/requirements-m1.txt)
-            else ()
-                set(requirements_txt ${genkiml_root}/requirements.txt)
-            endif ()
-
-            execute_process(COMMAND ${Python_EXECUTABLE} -m pip install -r ${requirements_txt})
-        endif()
+#        # Github actions can take care of caching pip dependencies
+#        # Also, this virtualenv approach doesn't seem to work properly there...
+#        if (NOT $ENV{GITHUB_ACTIONS})
+#            set(venv ${CMAKE_BINARY_DIR}/.venv)
+#
+#            if (NOT EXISTS ${venv})
+#                message("Creating virtual environment at ${venv}")
+#                execute_process(COMMAND ${Python_EXECUTABLE} -m venv ${venv})
+#            endif()
+#
+#            # A trick to make sure cmake uses the virtual environment we've created
+#            set(ENV{VIRTUAL_ENV} ${venv})
+#            set(Python_FIND_VIRTUALENV FIRST)
+#            unset (Python_EXECUTABLE)
+#            find_package(Python COMPONENTS Interpreter)
+#
+#            if (APPLE_M1)
+#                set(requirements_txt ${genkiml_root}/requirements-m1.txt)
+#            else ()
+#                set(requirements_txt ${genkiml_root}/requirements.txt)
+#            endif ()
+#
+#            execute_process(COMMAND ${Python_EXECUTABLE} -m pip install -r ${requirements_txt})
+#        endif()
 
         message("Python executable: ${Python_EXECUTABLE}")
 
