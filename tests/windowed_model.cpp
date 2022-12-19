43a066bc5ad5930c90b7edad10d488db41a5b559
Daniel Gretarsson <danielgretars@gmail.com>
2022-12-19 13:54:19 +0000

    Fix lambda capture issue (failed to compile on MSVC)

--- tests/windowed_model.cpp
+++ tests/windowed_model.cpp
@@ -11,17 +11,17 @@ ut::suite windowed_model = []
     using namespace ut;
     using namespace ranges::views;
 
-    constexpr auto test_linspace = [](auto min, auto max, auto count)
+    "linspace"_test = []
     {
-        auto ls = genki::ml::linspace(min, max, count);
+        constexpr auto test_linspace = [](auto min, auto max, auto count)
+        {
+            auto ls = genki::ml::linspace(min, max, count);
 
-        expect(ls.size() == count);
-        expect(ranges::all_of(zip(ls, ls | drop(1)),
-                [&](const auto& p) { return is_float_eq(p.second - p.first, (max - min) / (count - 1)); }));
-    };
+            expect(ls.size() == count);
+            expect(ranges::all_of(zip(ls, ls | drop(1)),
+                    [&](const auto& p) { return is_float_eq(p.second - p.first, (max - min) / (count - 1)); }));
+        };
 
-    "linspace"_test = [&]
-    {
         test_linspace(0.0, 1.0, 11);
         test_linspace(-5.0, 1.0, 13);
         test_linspace(100.0, 500.0, 1000);
